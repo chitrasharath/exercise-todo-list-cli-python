a504e5b8468099e801aeb0f3447e9a77ff1c858b
chitrasharath <chitrasharathchandra001@gmail.com>
2026-06-03 15:36:15 +0000

    Update app to use a class instead of global variables and add error checks

--- app.py
+++ app.py
@@ -1,54 +1,80 @@
 import csv
+import os
 
-todos = []
-stop = False
+
+class TodoApp:
+    def __init__(self):
+        self._todos = []
+
+    def get_todos(self):
+        return self._todos
+
+    def add_one_task(self, title):
+        self._todos.append(title)
+
+    def print_list(self):
+        for i in range(len(self._todos)):
+            print(str(i + 1) + ".  " + self._todos[i])
+
+    def delete_task(self, number_to_delete):
+        try:
+            number = int(number_to_delete)
+        except (ValueError, TypeError):
+            print("Enter number of task to delete")
+            return
+        if 1 <= number <= len(self._todos):
+            self._todos.pop(number - 1)
+        else:
+            print("That number does not exist")
+
+    def save_todos(self):
+        with open('todos.csv', 'w', newline='') as f:
+            writer = csv.writer(f)
+            for todo in self._todos:
+                writer.writerow([todo])
+
+    def load_todos(self):
+        if not os.path.isfile('todos.csv'):
+            print("There is no file")
+            return
+        self._todos = []
+        with open('todos.csv', 'r') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                self._todos.append(row[0])
+
+
+_app = TodoApp()
+
 
 def get_todos():
-    global todos
-    return todos
+    return _app.get_todos()
+
 
 def add_one_task(title):
-    # your code here
-    global todos
-    todos.append(title)
-    pass
+    _app.add_one_task(title)
+
 
 def print_list():
-    global todos
-    for i in range(len(todos)):
-        print(str(i+1) + ".  " + todos[i])
-    pass
+    _app.print_list()
+
 
 def delete_task(number_to_delete):
-    # your code here
-    global todos
-    if (int(number_to_delete) <= len(todos)):
-        todos.pop(int(number_to_delete) - 1)
-    pass
+    _app.delete_task(number_to_delete)
+
 
 def save_todos():
-    # your code here
-    global todos
-    with open('todos.csv', 'w', newline='') as f:
-        writer = csv.writer(f)
-        for todo in todos:
-            writer.writerow([todo])
-    pass
+    _app.save_todos()
+
 
 def load_todos():
-    # your code here
-    global todos
-    todos = []
-    with open('todos.csv', 'r') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            todos.append(row[0])
+    _app.load_todos()
 
-    pass
 
 # Below this code will only run if the entry file running was app.py
 if __name__ == '__main__':
-    while stop == False:
+    stop = False
+    while not stop:
         print("""
     Choose an option: 
         1. Add one task
@@ -62,11 +88,17 @@ if __name__ == '__main__':
         if response == "6":
             stop = True
         elif response == "3":
+            print()
             print_list()
         elif response == "2":
-            print("What task number you want to delete?")
-            number_to_delete = input()
-            delete_task(number_to_delete)
+            if len(get_todos()) == 0:
+                print("There are no tasks")
+            else:
+                print()
+                print_list()
+                print("What task number you want to delete?")
+                number_to_delete = input()
+                delete_task(number_to_delete)
         elif response == "1":
             print("What is your task title?")
             title = input()
